2db077732313fbd90e98801545585b33b169c7b1
LeeJeongSeok <ljs0429777@gmail.com>
2021-10-09 02:08:20 +0900

    기존에 AppConfig을 활용하여 DI 하던 부분을 스프링으로 전환함

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,7 +9,10 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig { // AppConfig 클래스는 배우를 캐스팅하는 일종의 공연 기획자라고 생각하자
 
     /**
@@ -17,21 +20,25 @@ public class AppConfig { // AppConfig 클래스는 배우를 캐스팅하는 일
      * AppConfig 리펙토링 강의 중 역할에 따른 구현이 잘 안보인다고 하는데..무슨 느낌인지 모르겠다..
      * 기존에 코드들을 보면 역할에 대한 구현이 한번에 뭉쳐서 이루어졌지만, 그것보단 각각의 역할별로 구현을 따로 만들어 하나의 역할에는 어떤 구현이 되어있는지 보기 쉽게 리펙토링한 것 같다.--> 내가 썻는데도 먼말인지 모르겠다
      */
+    @Bean
     public MemberService memberService() {
 //        return new MemberServiceImpl(new MemoryMemberRepository()); 리펙토링 전 코드
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
 //        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); 리펙토링 전 코드
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,16 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //name은 AppConfig 파일에 메소드명으로 찾으면 된다.
+
         Member member = new Member(1L, "memberA", Grade.VIP); // command + option + v
         memberService.join(member);
 
