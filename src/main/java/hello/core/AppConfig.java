ee73e8a0dbb5d69e1b5de71397bd396cf89fe214
LeeJeongSeok <ljs0429777@gmail.com>
2021-10-09 01:25:16 +0900

    AppConfig 리펙토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,27 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig { // AppConfig 클래스는 배우를 캐스팅하는 일종의 공연 기획자라고 생각하자
 
-    public MemberService memberService() { // 공연 기획자가 service라는 대본에 impl 이라는 어떤 배우를 캐스팅하는지 보여주는 코드라고 생각하자
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    /**
+     * 공연 기획자가 service라는 대본에 impl 이라는 어떤 배우를 캐스팅하는지 보여주는 코드라고 생각하자 (관심사 분리 강의 중)
+     * AppConfig 리펙토링 강의 중 역할에 따른 구현이 잘 안보인다고 하는데..무슨 느낌인지 모르겠다..
+     * 기존에 코드들을 보면 역할에 대한 구현이 한번에 뭉쳐서 이루어졌지만, 그것보단 각각의 역할별로 구현을 따로 만들어 하나의 역할에는 어떤 구현이 되어있는지 보기 쉽게 리펙토링한 것 같다.--> 내가 썻는데도 먼말인지 모르겠다
+     */
+    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository()); 리펙토링 전 코드
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); 리펙토링 전 코드
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+
 }
